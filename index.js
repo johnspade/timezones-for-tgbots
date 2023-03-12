d6eb5a4cc90735dbf3b28e98c39d8a75a8771d76
ilopatin <ivan@ilopatin.ru>
2023-03-12 15:32:47 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const dateTimeUtc = moment.utc();
+document.querySelector(".js-TimeUtc").innerHTML = dateTimeUtc.format("ddd, DD MMM YYYY HH:mm:ss");
+
+const selectorOptions = moment.tz.names()
+    .reduce((memo, tz) => {
+        memo.push({
+            name: tz,
+            offset: moment.tz(tz).utcOffset()
+        });
+
+        return memo;
+    }, [])
+    .sort((a, b) => {
+        return a.offset - b.offset
+    })
+    .reduce((memo, tz) => {
+        const timezone = tz.offset ? moment.tz(tz.name).format('Z') : '';
+
+        return memo.concat(`<option value="${tz.name}">(GMT${timezone}) ${tz.name}</option>`);
+    }, "");
+
+document.querySelector(".js-Selector").innerHTML = selectorOptions;
+
+document.querySelector(".js-Selector").addEventListener("change", e => {
+    const timestamp = dateTimeUtc.unix();
+    const offset = moment.tz(e.target.value).utcOffset() * 60;
+    const dateTimeLocal = moment.unix(timestamp + offset).utc();
+
+    document.querySelector(".js-TimeLocal").innerHTML = dateTimeLocal.format("ddd, DD MMM YYYY HH:mm:ss");
+});
+
+document.querySelector(".js-Selector").value = "Europe/London";
+
+const event = new Event("change");
+document.querySelector(".js-Selector").dispatchEvent(event);
